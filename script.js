ac336c0c670972ddc80c53ef601f9eb3640ff926
swishedmc-stack <swishedmc@gmail.com>
2026-05-08 19:08:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("Private Bot Test website loaded.");
+function showPage(id){
+
+  document.querySelectorAll(
+    '.login-page,.servers-page,.dashboard-page,.transcript-page'
+  ).forEach(page=>{
+    page.classList.add('hidden');
+  });
+
+  document.getElementById(id).classList.remove('hidden');
+}
+
+document.querySelectorAll('.nav').forEach(button=>{
+
+  button.addEventListener('click',()=>{
+
+    document.querySelectorAll('.nav').forEach(b=>{
+      b.classList.remove('active');
+    });
+
+    button.classList.add('active');
+
+    document.querySelectorAll('.dash-section').forEach(section=>{
+      section.classList.remove('active');
+    });
+
+    const page = button.dataset.page;
+
+    document.getElementById(page).classList.add('active');
+  });
+});
